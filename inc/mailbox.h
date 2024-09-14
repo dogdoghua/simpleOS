889847874929f6e0e2f36811b16c4f644f113e00
root <a0912938624@gmail.com>
2024-09-15 01:36:51 +0800

    move macro #define

--- inc/mailbox.h
+++ inc/mailbox.h
@@ -1,19 +1,25 @@
 #ifndef _MAILBOX_H_
 #define _MAILBOX_H_
-extern volatile unsigned int mbox[36];
 
-#define MBOX_REQUEST    0
+#define MAILBOX_BASE    (MMIO_BASE + 0x0000B880)
 
-/* channels */
-#define MBOX_CH_POWER   0
-#define MBOX_CH_FB      1
-#define MBOX_CH_VUART   2
-#define MBOX_CH_VCHIQ   3
-#define MBOX_CH_LEDS    4
-#define MBOX_CH_BTNS    5
-#define MBOX_CH_TOUCH   6
-#define MBOX_CH_COUNT   7
-#define MBOX_CH_PROP    8
+#define MAILBOX_READ    ((volatile unsigned int*)(MAILBOX_BASE))
+#define MAILBOX_STATUS  ((volatile unsigned int*)(MAILBOX_BASE + 0x18))
+#define MAILBOX_WRITE   ((volatile unsigned int*)(MAILBOX_BASE + 0x20))
 
-int mbox_call(char ch);
+#define MAILBOX_EMPTY   0x40000000
+#define MAILBOX_FULL    0x80000000
+
+#define GET_BOARD_REVISTION     0x00010002
+#define GET_ARM_MEM             0x00010005
+
+#define REQUEST_CODE            0x00000000
+#define REQUEST_SUCCEED         0x80000000
+#define TAG_REQUEST_CODE        0x00000000
+#define END_TAG                 0x00000000
+
+
+int mbox_request(unsigned int *mailbox);
+void get_board_revision();
+void get_arm_mem_info();
 #endif
