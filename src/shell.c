e01fed31eeec9f7d048c251d0ca965ae1f12c677
root <a0912938624@gmail.com>
2024-09-15 01:30:52 +0800

    add option for meminfo and boardinfo in shell

--- src/shell.c
+++ src/shell.c
@@ -2,6 +2,7 @@
 #include "string.h"
 #include "reboot.h"
 #include "sprintf.h"
+#include "mailbox.h"
 
 void shell_input(char *cmd) {
     uart_puts("\r# ");
@@ -63,14 +64,22 @@ void shell()
 		}
 		else if( strcmp(cmd, "help") == 0)
 		{
-			uart_puts("\nhelp\t: print this help menu\n");
-            uart_puts("hello\t: print Hello World!\n");
-            uart_puts("reboot\t: reboot the device\n");
+			uart_puts("\nhelp\t\t: print this help menu\n");
+            uart_puts("hello\t\t: print Hello World!\n");
+            uart_puts("reboot\t\t: reboot the device\n");
+            uart_puts("meminfo\t\t: get the base address and size of memory\n");
+            uart_puts("boardinfo\t: get the board revision number of raspi 3b\n");
 		}
 		else if( strcmp(cmd, "reboot") == 0)
 		{
 			uart_puts("\nGoing to reboot! See you later~~\n");
             reset(50);
+		}
+		else if (strcmp(cmd, "meminfo") == 0) {
+			get_arm_mem_info();
+		}
+		else if (strcmp(cmd, "boardinfo") == 0) {
+			get_board_revision();
 		}
 		else
 		{
