410504d7cd619f121c80ea2bfa088d3f755ef750
root <a0912938624@gmail.com>
2024-09-30 03:30:12 +0800

    modify: printf new line pattern

--- src/shell.c
+++ src/shell.c
@@ -1,11 +1,12 @@
 #include "uart.h"
 #include "string.h"
 #include "reboot.h"
-#include "sprintf.h"
+#include "utils.h"
 #include "mailbox.h"
+#include "cpio.h"
 
 void shell_input(char *cmd) {
-    uart_puts("\r# ");
+    uart_puts("# ");
     char c;
     int idx = 0, end = 0;
     cmd[0] = '\0';
@@ -46,9 +47,8 @@ void shell_input(char *cmd) {
             cmd[++end] = '\0';
         }
         printf("\r# %s \r\e[%dC", cmd, idx + 2);
-        //printf("%d %d\n", idx, end);
-        //printf("\r# %s \r", cmd);
     }
+	printf("\r");
 }
 void shell()
 {
@@ -58,11 +58,11 @@ void shell()
 	{
 		shell_input(cmd);
 
-		if( strcmp(cmd, "hello") == 0)
+		if (strcmp(cmd, "hello") == 0)
 		{
 			uart_puts("\nHello World!\n");
 		}
-		else if( strcmp(cmd, "help") == 0)
+		else if (strcmp(cmd, "help") == 0)
 		{
 			uart_puts("\nhelp\t\t: print this help menu\n");
             uart_puts("hello\t\t: print Hello World!\n");
@@ -70,7 +70,7 @@ void shell()
             uart_puts("meminfo\t\t: get the base address and size of memory\n");
             uart_puts("boardinfo\t: get the board revision number of raspi 3b\n");
 		}
-		else if( strcmp(cmd, "reboot") == 0)
+		else if (strcmp(cmd, "reboot") == 0)
 		{
 			uart_puts("\nGoing to reboot! See you later~~\n");
             reset(50);
@@ -81,8 +81,13 @@ void shell()
 		else if (strcmp(cmd, "boardinfo") == 0) {
 			get_board_revision();
 		}
-		else
-		{
+		else if (strcmp(cmd, "ls") == 0) {
+			cpio_list();
+		}
+		else if (strcmp(cmd, "cat") == 0) {
+			cpio_cat();
+		}
+		else {
 			uart_puts("\nCommand not found \n");
 		}
 	}
